c092515aff399f6c592a593ff1fd1a78bf206f52
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-08-23 23:33:25 +0530

    basics for greet app made

--- greetapp/src/index.js
+++ greetapp/src/index.js
@@ -2,9 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+var msg = "Good Morning";
+// let currtime = new Date().toLocaleTimeString();
+let currtime = new Date().getHours();
+let timecss = {
+  color: "black",
+};
+
+if (currtime > 12 && currtime <= 16) {
+  msg = "Good Afternoon";
+  timecss.color = "yellow";
+} else if (currtime > 16 && currtime <= 20) {
+  msg = "Good Evening";
+  timecss.color = "blue";
+} else if (currtime > 20 && currtime <= 24) {
+  msg = "Good Night";
+  timecss.color = "purple";
+} else {
+  msg = "Good Morning";
+  timecss.color = "green";
+}
+
 ReactDOM.render(
   <>
-    <h1>hello world</h1>
+    <div className="greetmsg">
+      <h1>
+        hello User ,<span style={timecss}>{msg} </span>
+      </h1>
+    </div>
   </>,
   document.getElementById("root")
 );
